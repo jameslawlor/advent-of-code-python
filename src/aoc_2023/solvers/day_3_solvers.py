8d898455eeeec8b48e063f939a31f6293b6eb624
James <james@Susans-MacBook-Air.local>
2023-12-27 14:54:08 +0000

    linting

--- src/aoc_2023/solvers/day_3_solvers.py
+++ src/aoc_2023/solvers/day_3_solvers.py
@@ -2,7 +2,6 @@ import re
 
 
 class Part:
-
     def __init__(self, part_number):
         self.part_number = str(part_number)
         self.coordinates = []
@@ -11,13 +10,13 @@ class Part:
     def set_coordinates(self, y_pos, x_start_pos, x_end_pos):
         if x_start_pos > x_end_pos:
             raise ValueError(
-                "Invalid coordinates: x_start_pos should be less than or equal to x_end_pos"
-                )
+                "Invalid coordinates: "
+                "x_start_pos should be less than or equal to x_end_pos"
+            )
         self.x_start_coordinate = x_start_pos
         self.x_end_coordinate = x_end_pos
         self.y_coordinate = y_pos
-        self.full_coordinates = [(y_pos, x)  for x in range(x_start_pos, x_end_pos+1)]
-
+        self.full_coordinates = [(y_pos, x) for x in range(x_start_pos, x_end_pos + 1)]
 
     def part_number_as_integer(self):
         try:
@@ -25,12 +24,12 @@ class Part:
         except (ValueError, TypeError):
             # Handle the case where part_number is not a valid integer
             return None
-        
+
     def get_window_around_part(self):
         if self.full_coordinates is None:
             raise ValueError(
                 "Part has no full_coordinates! Check Part initialised correctly."
-                )
+            )
 
         self.window = []
         for y in [self.y_coordinate - 1, self.y_coordinate, self.y_coordinate + 1]:
@@ -42,17 +41,17 @@ class Part:
 
 
 class Symbol:
-    def __init__(self,symbol=None,coordinates=None):
+    def __init__(self, symbol=None, coordinates=None):
         self.symbol = symbol
         self.coordinates = coordinates
         self.is_gear = False
-    
+
     def set_coordinates(self, y_pos, x_pos):
         self.coordinates = (y_pos, x_pos)
 
     def set_symbol(self, char):
         self.symbol = char
-        self.is_potential_gear = (self.symbol == "*")
+        self.is_potential_gear = self.symbol == "*"
 
     def mark_as_gear(self):
         self.is_gear = True
@@ -95,23 +94,23 @@ def identify_part_numbers(input, symbols_collection) -> list:
             part_number = match.group()
             part = Part(
                 part_number=part_number,
-
-                )
+            )
             part.set_coordinates(line_number, match_start, match_end)
             part.get_window_around_part()
             if part.check_window_for_symbol(symbol_positions):
                 parts_list.append(part)
-    
+
     return parts_list
 
 
 def identify_gears(parts_collection, symbol_collection):
     # for sym in symbol_collection:
-        # get window around symbol
+    # get window around symbol
 
-        #
+    #
 
     return
+
 
 def calculate_sum_of_part_numbers(input_list) -> int:
     return sum([part.part_number_as_integer() for part in input_list])
@@ -121,6 +120,6 @@ def solve_day_3(input) -> int:
     symbol_collection = get_symbols(input)
     parts_collection = identify_part_numbers(input, symbol_collection)
     sum_of_part_numbers = calculate_sum_of_part_numbers(parts_collection)
-    gears_collection = identify_gears(parts_collection, symbol_collection)
+    # gears_collection = identify_gears(parts_collection, symbol_collection)
     # sum_of_gear_ratios = calculate_sum_of_gear_ratios()
     return sum_of_part_numbers
--- tests/test_day_3_solvers.py
+++ tests/test_day_3_solvers.py
@@ -6,11 +6,9 @@ from aoc_2023.solvers.day_3_solvers import (
     check_char_is_number,
     check_char_is_symbol,
     get_symbols,
-    check_window_for_symbol,
     identify_part_numbers,
     calculate_sum_of_part_numbers,
     solve_day_3,
-    identify_gears,
 )
 
 
@@ -20,9 +18,11 @@ def test_set_coordinates():
     pc.set_coordinates(1, 2, 4)
     assert pc.full_coordinates == [(1, 2), (1, 3), (1, 4)]
 
+
 def test_set_part_number():
     pc = Part(part_number=123)
-    assert pc.part_number == '123'
+    assert pc.part_number == "123"
+
 
 def test_part_number_as_integer():
     pc = Part(part_number="456")
@@ -36,9 +36,10 @@ def test_part_number_as_integer():
     assert result is None
 
     # Test conversion when part_number is not a valid integer string
-    pc.part_number = 'abc'
+    pc.part_number = "abc"
     result = pc.part_number_as_integer()
     assert result is None
+
 
 # Additional test case to demonstrate input validation in set_coordinates
 def test_set_coordinates_invalid_input():
@@ -52,10 +53,12 @@ def test_set_symbol_coordinates():
     sym.set_coordinates(1, 2)
     assert sym.coordinates == (1, 2)
 
+
 def test_set_symbol_symbol():
     sym = Symbol()
-    sym.set_symbol('X')
-    assert sym.symbol == 'X'
+    sym.set_symbol("X")
+    assert sym.symbol == "X"
+
 
 # Additional test cases to check default values
 def test_symbol_default_values():
@@ -63,50 +66,60 @@ def test_symbol_default_values():
     assert sym.coordinates is None
     assert sym.symbol is None
 
-# Additional test case to check if set_coordinates and set_symbol can be called independently
+
+# Additional test case to check if set_coordinates
+# and set_symbol can be called independently
 def test_symbol_independent_calls():
     sym = Symbol()
     sym.set_coordinates(3, 4)
     assert sym.coordinates == (3, 4)
     assert sym.symbol is None  # symbol should remain unchanged
 
-    sym.set_symbol('O')
+    sym.set_symbol("O")
     assert sym.coordinates == (3, 4)  # coordinates should remain unchanged
-    assert sym.symbol == 'O'
+    assert sym.symbol == "O"
+
 
 # Test cases for check_char_is_number function
 def test_check_char_is_number_with_number():
-    result = check_char_is_number('5')
+    result = check_char_is_number("5")
     assert result is True
 
+
 def test_check_char_is_number_with_non_number():
-    result = check_char_is_number('*')
+    result = check_char_is_number("*")
     assert result is False
 
+
 def test_check_char_is_number_with_dot():
-    result = check_char_is_number('.')
+    result = check_char_is_number(".")
     assert result is False
+
 
 # Test cases for check_char_is_symbol function
 def test_check_char_is_symbol_with_symbol():
-    result = check_char_is_symbol('$')
+    result = check_char_is_symbol("$")
     assert result is True
+
 
 def test_check_char_is_symbol_with_number():
-    result = check_char_is_symbol('7')
+    result = check_char_is_symbol("7")
     assert result is False
+
 
 def test_check_char_is_symbol_with_dot():
-    result = check_char_is_symbol('.')
+    result = check_char_is_symbol(".")
     assert result is False
 
+
 def test_check_char_is_symbol_with_gear():
-    result = check_char_is_symbol('*')
+    result = check_char_is_symbol("*")
     assert result is True
+
 
 # Additional test case to cover an empty string
 def test_check_char_is_symbol_with_empty_string():
-    result = check_char_is_symbol('')
+    result = check_char_is_symbol("")
     assert result is False
 
 
@@ -125,52 +138,61 @@ def day_3_test_input():
         ".664.598..",
     ]
 
+
 @pytest.fixture
 def day_3_expected_symbols_collection():
     return [
-        Symbol("*",(1, 3)),
-        Symbol("#",(3, 6)),
-        Symbol("*",(4, 3)),
-        Symbol("+",(5, 5)),
-        Symbol("$",(8, 3)),
-        Symbol("*",(8, 5)),
+        Symbol("*", (1, 3)),
+        Symbol("#", (3, 6)),
+        Symbol("*", (4, 3)),
+        Symbol("+", (5, 5)),
+        Symbol("$", (8, 3)),
+        Symbol("*", (8, 5)),
     ]
-
 
 
 @pytest.fixture
 def day_3_expected_parts_collection():
     return [
-    Part("467"), 
-    Part("35"), 
-    Part("633"), 
-    Part("617"), 
-    Part("592"), 
-    Part("755"), 
-    Part("664"), 
-    Part("598")
+        Part("467"),
+        Part("35"),
+        Part("633"),
+        Part("617"),
+        Part("592"),
+        Part("755"),
+        Part("664"),
+        Part("598"),
     ]
-        
-        
-def get_symbols(day_3_test_input, day_3_expected_symbols_collection):
+
+
+def test_get_symbols(day_3_test_input, day_3_expected_symbols_collection):
     test_symbols_list = get_symbols(day_3_test_input)
     output_symbol_positions = [sym.coordinates for sym in test_symbols_list]
-    assert output_symbol_positions == day_3_expected_symbols_collection
-
+    expected_symbol_positions = [
+        sym.coordinates for sym in day_3_expected_symbols_collection
+    ]
+    assert output_symbol_positions == expected_symbol_positions
 
 
 def test_identify_part_numbers(
     day_3_test_input, day_3_expected_symbols_collection, day_3_expected_parts_collection
 ):
-    test_parts_collection = identify_part_numbers(day_3_test_input, day_3_expected_symbols_collection)
-    test_part_numbers = [part.part_number for part in test_parts_collection]
-    day_3_expected_part_numbers = [part.part_number for part in day_3_expected_parts_collection]
-    assert ( test_part_numbers == day_3_expected_part_numbers
+    test_parts_collection = identify_part_numbers(
+        day_3_test_input, day_3_expected_symbols_collection
     )
+    test_part_numbers = [part.part_number for part in test_parts_collection]
+    day_3_expected_part_numbers = [
+        part.part_number for part in day_3_expected_parts_collection
+    ]
+    assert test_part_numbers == day_3_expected_part_numbers
 
 
-def test_calculate_sum_of_part_numbers(day_3_test_input, day_3_expected_symbols_collection):
-    test_parts_list = identify_part_numbers(day_3_test_input, day_3_expected_symbols_collection)
+def test_calculate_sum_of_part_numbers(
+    day_3_test_input, day_3_expected_symbols_collection
+):
+    test_parts_list = identify_part_numbers(
+        day_3_test_input, day_3_expected_symbols_collection
+    )
     assert calculate_sum_of_part_numbers(test_parts_list) == 4361
 
 
@@ -200,4 +222,3 @@ def test_custom_input(day_3_test_input_custom):
 #             )
 #         ]
 #     )
-
