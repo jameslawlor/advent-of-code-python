886a55ad6a6abacf5c44ee5b22e34231af939c7d
James <james@Susans-MacBook-Air.local>
2023-12-27 19:45:08 +0000

    day 5 init

--- src/aoc_2023/days/5.py
+++ src/aoc_2023/days/5.py
@@ -1,17 +1,15 @@
-from aoc_2023.solvers.day_4_solvers import (
-    solve_day_4,
+from aoc_2023.solvers.day_5_solvers import (
+    solve_day_5,
 )
 from aoc_2023.utils.input_handling import read_input, parse_args
-
 
 def main():
     args = parse_args()
     input = read_input(args.input_file)
-    result_part_1, result_part_2 = solve_day_4(input)
+    result_part_1 = solve_day_5(input)
     print(
-        f"Day 4: "
+        f"Day 5: "
         f" Total points for part 1 is {result_part_1}. "
-        f" Total points for part 2 is {result_part_2}. "
     )
 
 
---
+++ src/aoc_2023/solvers/day_5_solvers.py
@@ -0,0 +1,3 @@
+
+def solve_day_5(input) -> int:
+    return 0
