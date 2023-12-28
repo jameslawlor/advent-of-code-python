b4662a2f664dca9ca6b56c9116365298b43af56b
James <james@Susans-MacBook-Air.local>
2023-12-28 10:52:24 +0000

    day 6 part 1 solution

---
+++ src/aoc_2023/solvers/day_6_solvers.py
@@ -0,0 +1,58 @@
+import math
+import re
+
+class Race:
+    def __init__(self, time, distance_to_beat):
+        self.time = int(time)
+        self.distance_to_beat = int(distance_to_beat)
+        self._compute_ways_to_win()
+
+    def __str__(self):
+        return (
+            f"time: {self.time} | "
+            f"distance_to_beat: {self.distance_to_beat} | "
+        )
+    
+    def _compute_ways_to_win(self):
+        possible_speeds = range(self.time+1)
+        self.ways_to_win = 0
+        for possible_speed in possible_speeds:
+            time_remaining = self.time - possible_speed
+            distance_travelled = possible_speed*time_remaining
+            if distance_travelled > self.distance_to_beat:
+                self.ways_to_win+=1
+    
+class Races:
+    def __init__(self, *races):
+        self.races = list(races)
+
+    def __str__(self):
+        return ("".join([str(race) for race in self.races]))
+    
+    def n_races(self):
+        return len(self.races)
+
+    def add_race(self, race):
+        self.races.append(race)
+    
+    def solve(self):
+        return math.prod([race.ways_to_win for race in self.races])
+
+
+def create_races(input):
+    
+    races = Races()
+
+    times = re.findall(r"(\d+)", input[0])
+    distances = re.findall(r"(\d+)", input[1])
+
+    for time, distance in zip(times,distances):
+        race = Race(time=time,distance_to_beat=distance)
+        races.add_race(race)
+    
+    return races
+
+
+def solve_day_6(input):
+    races = create_races(input)
+    return races.solve()
