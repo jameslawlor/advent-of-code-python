533c79cf03c57c7e65d96bc2be8a2cd63c75f62e
James Lawlor <jameslawlor1987@gmail.com>
2025-12-03 12:18:31 +0200

    refactor tests

--- tests/year_2025/test_day_01.py
+++ tests/year_2025/test_day_01.py
@@ -2,7 +2,7 @@ import pytest
 from advent_of_code.year_2025.day_01 import (
     solve,
     turn_dial,
-    passing_zero_counter,
+    position_counter,
 )
 
 
@@ -24,45 +24,58 @@ def day_01_test_input():
 
 @pytest.fixture
 def day_01_expected_output():
-    return (3, None)
+    return (3, 6)
 
 
 def test_solver(day_01_test_input, day_01_expected_output):
     result = solve(day_01_test_input)
     assert result == day_01_expected_output
 
-@pytest.mark.parametrize(
-    "current_position, turn_direction, distance, expected_new_position",
-    [
-        (50, -1, 68, 82),
-        (82, -1, 30, 52),
-        (52, 1, 48, 0), 
-        (0, -1, 5, 95),
-        (95, 1, 60, 55),
-        (55, -1, 55, 0),
-        (0, -1, 1, 99),
-        (99, -1, 99, 0),
-        (0, 1, 14, 14),
-        (14, -1, 82, 32),
+@pytest.fixture(params=[
+        (50 ,    -1    , 68 , 82    , 0 , 1 ),
+        (82 ,    -1    , 30 , 52    , 0 , 0 ),
+        (52 ,    1     , 48 , 0     , 1 , 1 ), 
+        (0  ,    -1    , 5  , 95    , 0 , 0 ),
+        (95 ,    1     , 60 , 55    , 0 , 1 ),
+        (55 ,    -1    , 55 , 0     , 1 , 1 ),
+        (0  ,    -1    , 1  , 99    , 0 , 0 ),
+        (99 ,    -1    , 99 , 0     , 1 , 1 ),
+        (0  ,    1     , 14 , 14    , 0 , 0 ),
+        (14 ,    -1    , 82 , 32    , 0 , 1 ),
     ],
 )
-def test_turn_dial(current_position, turn_direction, distance, expected_new_position):
+def turn_case(request):
+    """
+    (current_position, turn_direction, distance, expected_new_position, part_1_count, part_2_count)
+
+    """
+    return request.param
+
+def test_turn_dial(turn_case):
+    current_position, turn_direction, distance, expected_new_position, _, _ = turn_case
     new_position = turn_dial(current_position, turn_direction, distance)
     assert new_position == expected_new_position
 
-@pytest.mark.parametrize(
-    "start_position, turn_direction, distance, expected_count",
-    [
-        (50, 1, 1000, 10),
-        # (50, 1, 49, 0),
-        # (50, -1, 49, 0),
-        # (50, -1, 50, 1),
-        # (50, -1, 51, 1),
-        # (99, 1, 1, 1),
-        # (99, -1, 1, 0),
-        # (0, 1, 100, 1),
-    ]
-)
-def test_passing_zero_counter(start_position, turn_direction, distance, expected_count):
-    count = passing_zero_counter(start_position, turn_direction, distance)
-    assert count == expected_count
+
+def test_position_counter(turn_case):
+    _, _, _, expected_new_position, expected_count, _ = turn_case
+    actual_count = position_counter(expected_new_position)
+    assert actual_count == expected_count
+
+
+# @pytest.mark.parametrize(
+#     "start_position, turn_direction, distance, expected_count",
+#     [
+#         (50, 1, 1000, 10),
+#         # (50, 1, 49, 0),
+#         # (50, -1, 49, 0),
+#         # (50, -1, 50, 1),
+#         # (50, -1, 51, 1),
+#         # (99, 1, 1, 1),
+#         # (99, -1, 1, 0),
+#         # (0, 1, 100, 1),
+#     ]
+# )
+# def test_passing_zero_counter(start_position, turn_direction, distance, expected_count):
+#     count = passing_zero_counter(start_position, turn_direction, distance)
+#     assert count == expected_count
