3da786c8bb1ca0dd4d64d294c1605171e8f5e04c
James Lawlor <jameslawlor1987@gmail.com>
2025-12-07 13:49:41 +0200

    2025-2-2 (#22)

--- src/advent_of_code/year_2025/day_02.py
+++ src/advent_of_code/year_2025/day_02.py
@@ -1,5 +1,6 @@
 from advent_of_code.utils.input_handling import read_input
 
+VALID_PARTS = (1,2)
 
 def parse_input(raw):
     raw_split = raw[0].split(",")
@@ -24,26 +25,44 @@ def check_id_valid(id):
     else:
         return True
 
+def check_id_valid_v2(id):
+    id_len = len(str(id))
+    half_len = id_len // 2
+    for sub_str_len in range(1,half_len+1):
+        sub_str = id[:sub_str_len]
+        str_to_search = id[sub_str_len:]
+        if (len(str_to_search) % sub_str_len == 0): # substr divisible by search str length
+            if (sub_str * (id_len // sub_str_len) == id): # substr matches N * search string 
+                return False
+    return True
 
-def get_invalid_ids_in_range(range_pair):
+def get_invalid_ids_in_range(range_pair, part):
     full_range = range(range_pair[0], range_pair[1] + 1)
+
+    if part == 1:
+        id_checker_function = check_id_valid
+    elif part == 2:
+        id_checker_function = check_id_valid_v2
+    else:
+        raise ValueError("Specify a valid part number")
+    
     invalid_ids = []
     for x in full_range:
-        if not check_id_valid(str(x)):
+        if not id_checker_function(str(x)):
             invalid_ids.append(x)
     return invalid_ids
 
 
-def find_invalid_ids_in_range_string(range_string):
+def find_invalid_ids_in_range_string(range_string, part):
     range_pair_as_ints = convert_range_string_to_pair(range_string)
-    invalid_ids_in_range = get_invalid_ids_in_range(range_pair_as_ints)
+    invalid_ids_in_range = get_invalid_ids_in_range(range_pair_as_ints, part)
     return invalid_ids_in_range
 
 
-def find_all_invalid_ids(parsed_input):
+def find_all_invalid_ids(parsed_input, part):
     invalid_ids = []
     for range_pair in parsed_input:
-        invalid_ids.extend(find_invalid_ids_in_range_string(range_pair))
+        invalid_ids.extend(find_invalid_ids_in_range_string(range_pair, part))
     return invalid_ids
 
 
@@ -51,14 +70,17 @@ def sum_invalid_ids(invalid_ids):
     return sum(invalid_ids)
 
 
-def solve_part_1(parsed_input):
+def solve_part(parsed_input, part):
+    invalid_ids = find_all_invalid_ids(parsed_input, part)
+    return sum_invalid_ids(invalid_ids)
+
+def solve_part_2(parsed_input):
     invalid_ids = find_all_invalid_ids(parsed_input)
     return sum_invalid_ids(invalid_ids)
 
-
 def solve(parsed_input):
-    part_1_solution = solve_part_1(parsed_input)
-    part_2_solution = None
+    part_1_solution = solve_part(parsed_input, part=1)
+    part_2_solution = solve_part(parsed_input, part=2)
     return (part_1_solution, part_2_solution)
 
 
