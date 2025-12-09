6611c8c4c129314f73821e98c836e0ea925c2490
James Lawlor <jameslawlor1987@gmail.com>
2025-12-09 09:03:51 +0200

    2025d3-2

--- src/advent_of_code/year_2025/day_03.py
+++ src/advent_of_code/year_2025/day_03.py
@@ -4,44 +4,49 @@ def parse_battery_bank_to_ints_list(battery_bank_str):
     return [int(x) for x in battery_bank_str]
 
 
-def combine_joltages(j1, j2):
-    return int(f"{j1}{j2}")
+def combine_joltages(list_of_joltages):
+    return int("".join([str(x) for x in list_of_joltages]))
 
 
 def calculate_largest_joltage(battery_bank_as_ints):
     largest_joltage_1 = 0
     
-
-    print(f"Calculating largest joltage for battery bank: {battery_bank_as_ints}")
-
     for battery_index, battery_value in enumerate(battery_bank_as_ints):
-     print(f"battery_value={battery_value}, battery_index={battery_index}")
      if (battery_value > largest_joltage_1) and (battery_index < len(battery_bank_as_ints) - 1):
          largest_joltage_1 = battery_value
          largest_joltage_2 = 0
-         print(f"Larger value found! Setting largest_joltage_1 to {largest_joltage_1}")
          for second_battery_value in battery_bank_as_ints[battery_index+1:]:
-             print(f"second_battery_value={second_battery_value}")
              if second_battery_value > largest_joltage_2:
                  largest_joltage_2 = second_battery_value
-                 print(f"Larger value found! Setting largest_joltage_2 to {largest_joltage_2}")
-
-    print(f"Largest joltage combination: {largest_joltage_1}, {largest_joltage_2}")
     
-    return combine_joltages(largest_joltage_1, largest_joltage_2)
+    return combine_joltages([largest_joltage_1, largest_joltage_2])
 
-def solve_part_1(input):
+
+def calculate_largest_joltage_part_2(battery_bank_as_ints, n_batteries=12):
+    sorted_batteries = sorted(battery_bank_as_ints, reverse=True)
+    return combine_joltages(sorted_batteries[:n_batteries])
+
+
+def solve_part(input, part=1):
     joltage_list = []
+
+    if part == 1:
+        calculate_largest_joltage_function = calculate_largest_joltage
+    elif part == 2:
+        calculate_largest_joltage_function = calculate_largest_joltage_part_2
+    else:
+        raise ValueError("Part must be 1 or 2")
+    
     for battery_bank_str in input:
         battery_bank_as_ints = parse_battery_bank_to_ints_list(battery_bank_str)
-        largest_joltage = calculate_largest_joltage(battery_bank_as_ints)
+        largest_joltage = calculate_largest_joltage_function(battery_bank_as_ints)
         joltage_list.append(largest_joltage)
 
     return sum(joltage_list)
 
 def solve(input):
-    part_1_solution = solve_part_1(input)
-    part_2_solution = None
+    part_1_solution = solve_part(input, part=1)
+    part_2_solution = solve_part(input, part=2)
     return (part_1_solution, part_2_solution)
 
 
--- tests/year_2025/test_day_03.py
+++ tests/year_2025/test_day_03.py
@@ -5,6 +5,7 @@ from advent_of_code.year_2025.day_03 import (
     calculate_largest_joltage,
     parse_battery_bank_to_ints_list,
     combine_joltages,
+    calculate_largest_joltage_part_2,
 )
 
 
@@ -20,29 +21,38 @@ def day_03_test_input():
 
 @pytest.fixture
 def day_03_expected_output():
-    return (357, None)
+    return (357, 3121910778619)
 
 
 def test_solver(day_03_test_input, day_03_expected_output):
     result = solve(day_03_test_input)
     assert result == day_03_expected_output
 
-@pytest.mark.parametrize(
-    "input, expected_output",
-    [
-        [[9,8,7,6,5,4,3,2,1,1,1,1,1,1,1],98],
-        [[8,1,1,1,1,1,1,1,1,1,1,1,1,1,9],89],
-        [[2,3,4,2,3,4,2,3,4,2,3,4,2,7,8],78],
-        [[8,1,8,1,8,1,9,1,1,1,1,2,1,1,1],92],
-    ],
-)
-def test_calculate_largest_joltage(input, expected_output):
-    parsed_inputs = parse_battery_bank_to_ints_list(input)
-    assert expected_output == calculate_largest_joltage(parsed_inputs)
+
+
+@pytest.fixture
+def test_calculate_largest_joltage_params():
+    return [
+        ("987654321111111", 98, 987654321111),
+        ("811111111111119", 89, 811111111119),
+        ("234234234234278", 78, 434234234278),
+        ("818181911112111", 92, 888911112111),
+    ]
+
+def test_calculate_largest_joltage_part_1(test_calculate_largest_joltage_params):
+    for input_str, expected_output, _ in test_calculate_largest_joltage_params:
+        parsed_inputs = parse_battery_bank_to_ints_list(input_str)
+        assert expected_output == calculate_largest_joltage(parsed_inputs)
+
+
+def test_calculate_largest_joltage_part_2(test_calculate_largest_joltage_params):
+    for input_str, _, expected_output in test_calculate_largest_joltage_params:
+        parsed_inputs = parse_battery_bank_to_ints_list(input_str)
+        assert expected_output == calculate_largest_joltage_part_2(parsed_inputs)
 
 
 def test_parse_battery_bank_to_ints_list():
     assert parse_battery_bank_to_ints_list("98765") == [9,8,7,6,5]
 
 def test_combine_joltages():
-    assert combine_joltages(9, 8) == 98
+    assert combine_joltages([9, 8, 7]) == 987
